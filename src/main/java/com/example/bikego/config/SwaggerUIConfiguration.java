c33b4bb96c98fb693c64cffbd742df13047c0bee
Duy Le <duyle210101@gmail.com>
2023-06-07 16:26:51 +0700

    config swagger ui

---
+++ src/main/java/com/example/bikego/config/SwaggerUIConfiguration.java
@@ -0,0 +1,33 @@
+package com.example.bikego.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerUIConfiguration {
+    @Bean
+    public OpenAPI myOpenAPI() {
+
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:5000");
+        localServer.setDescription("Server URL in Local environment");
+
+        Server productionServer = new Server();
+        productionServer.setUrl("");
+        productionServer.setDescription("Server URL in Production environment");
+
+        Info info = new Info()
+                .title("BIKEGO MANAGER API")
+                .version("1.0")
+                .description("FOR EDUCATION PURPOSE ONLY");
+
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(localServer, productionServer));
+    }
+}
