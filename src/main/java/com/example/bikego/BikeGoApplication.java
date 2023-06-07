bc5a14c73b5463238c0ddf93e311269824fd99cb
Duy Le <duyle210101@gmail.com>
2023-06-07 16:56:38 +0700

    config swagger ui v1.2

--- src/main/java/com/example/bikego/BikeGoApplication.java
+++ src/main/java/com/example/bikego/BikeGoApplication.java
@@ -2,8 +2,10 @@ package com.example.bikego;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class BikeGoApplication {
 
     public static void main(String[] args) {
