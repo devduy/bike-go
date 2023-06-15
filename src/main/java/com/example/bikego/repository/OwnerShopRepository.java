d3602700f994468ee76e1c9b1b7fbb23c106033d
Duy Le <duyle210101@gmail.com>
2023-06-15 22:55:42 +0700

    add id to owner shop dto

--- src/main/java/com/example/bikego/repository/OwnerShopRepository.java
+++ src/main/java/com/example/bikego/repository/OwnerShopRepository.java
@@ -2,10 +2,18 @@ package com.example.bikego.repository;
 
 import com.example.bikego.entity.OwnerShop;
 import com.example.bikego.entity.User;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OwnerShopRepository extends JpaRepository<OwnerShop, Long> {
     OwnerShop findByName(String name);
 
     OwnerShop findByUser(User user);
+
+    @NotNull
+    @Query("SELECT o from OwnerShop o where o.user.role.id  = 2")
+    List<OwnerShop> findAll();
 }
